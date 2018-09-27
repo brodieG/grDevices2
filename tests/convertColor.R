5ed34b3b92a3790b60ae3d4d351d67a964960099
brodieG <brodieG@users.noreply.github.com>
2018-09-26 22:12:07 -0400

    adding test structure

---
+++ tests/convertColor.R
@@ -0,0 +1,35 @@
+## These tests are not intended to become part of the patch
+
+# - Sample Input Spaces --------------------------------------------------------
+
+source('tests/utils.R')
+
+ranges.raw <- c(
+  0,   1,   0,   1,   0,   1,  # rgb*/xyz
+  0,   1,   0,   1,   0,   1,  # rgb*/xyz
+  0,   1,   0,   1,   0,   1,  # rgb*/xyz
+  0,   1,   0,   1,   0,   1,  # rgb*/xyz
+  0, 100,-128, 128,-128, 128,  # Lab
+  # Luv; supposed to be +- 100, but rgb conv suggests it's wider than that
+  0, 100,-180, 180,-180, 180
+)
+spaces <- c('Apple RGB', 'sRGB', 'CIE RGB', 'XYZ', 'Lab', 'Luv')
+
+ranges <- array(
+  ranges.raw, dim=c(2, 3, length(ranges.raw) / (2 * 3)),
+  dimnames=list(range=c('lo', 'hi'), NULL, space=spaces)
+)
+# space.sub <- c('sRGB', 'XYZ', 'Lab', 'Luv')
+space.sub <- spaces
+ranges.sub <- ranges[,,match(space.sub, space.sub)]
+
+# For each input space, generate permutation of values in range, outside of
+# range, along with NA, NaN, Inf, -Inf cases.
+
+space.input <- interpolate_space(ranges.sub, na=FALSE, inf=FALSE, steps=32)
+
+# - Compare Convert Color ------------------------------------------------------
+
+cc0 <- color_to_color(space.input, fun=grDevices0::convertColor)
+cc1 <- color_to_color(space.input, fun=grDevices1::convertColor)
+
---
+++ tests/utils.R
@@ -0,0 +1,60 @@
+## Run all permutations of colorspace to colorspace translation
+
+color_to_color <- function(
+  col, fun, spaces=names(col), strip.names=TRUE, time=FALSE
+) {
+  from.to <- subset(
+    do.call(expand.grid, list(from=spaces, to=spaces, stringsAsFactors=FALSE)),
+    from != to
+  )
+  fun_t <- function(...) {
+    res <- try(if(time) system.time(fun(...))[3] else fun(...), silent=TRUE)
+    if(inherits(res, 'try-error')) res <- 'error'
+    if(strip.names) unname(res) else res
+  }
+  args <- c(list(fun_t, color=col[from.to$from]), from.to)
+  res.raw <- do.call(Map, args)
+  res <- matrix(
+    if(time) numeric() else list(),
+    nrow=length(spaces), ncol=length(spaces),
+    dimnames=list(from=spaces, to=spaces)
+  )
+  if(time) res[as.matrix(from.to)] <- as.numeric(res.raw)
+  else res[as.matrix(from.to)] <- res.raw
+  res
+}
+## Given boundaries in a series of 3D spaces, interpolate points within them
+##
+## @param ranges a 2 x 3 x n array of the ranges, where n is the number of
+##   colorspaces we want to interpolate in, the first dimension is the start and
+##   end of the range, the second dimension represents the 3 dimensions of the
+##   colorspace, and the last dimension the colorspaces.
+
+interpolate_space <- function(
+  ranges, steps=16, expand=c(0.2, 1e6), na=TRUE, inf=TRUE
+) {
+  stopifnot(
+    identical(head(dim(ranges), 2), c(2L, 3L)), length(dim(ranges)) == 3
+  )
+  res <-lapply(seq_len(dim(ranges)[3]),
+    function(x) {
+      ranges <- split(ranges[,,x], col(ranges[,,x]))
+      ranges.ex <- lapply(
+        ranges,
+        function(y) {
+          c(
+            seq(from=y[1], to=y[2], length.out=steps),
+            min(y) - diff(range(y)) * expand,
+            max(y) + diff(range(y)) * expand,
+            if(na) c(NA, NaN), if(inf) c(Inf, -Inf)
+          )
+        }
+      )
+      do.call(cbind, as.list(do.call(expand.grid, ranges.ex)))
+    }
+  )
+  names(res) <- dimnames(ranges)[[3]]
+  res
+}
+
+
